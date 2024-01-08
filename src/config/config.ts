803aebb12eb14320fd4aac3865caeffed1bed33c
genebit <genebitara@gmail.com>
2024-01-08 16:16:16 +0800

    feat: add init firebase config

---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyC4Jko8KkTDFZCkV0wHt2PfInHFM9gOw_s",
+  authDomain: "gb-plantasker.firebaseapp.com",
+  projectId: "gb-plantasker",
+  storageBucket: "gb-plantasker.appspot.com",
+  messagingSenderId: "1056064395667",
+  appId: "1:1056064395667:web:adb80b20f245f068aa3066",
+  measurementId: "G-6ZL11EENCR",
+}
---
+++ src/utils/utils.tsx
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import { firebaseConfig } from "../config/config"
+
+export const initializeFirebase = () => {
+  // Initialize Firebase
+  initializeApp(firebaseConfig)
+  const app = initializeApp(firebaseConfig)
+  const analytics = getAnalytics(app)
+}
