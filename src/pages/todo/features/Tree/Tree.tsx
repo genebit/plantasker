51e83610b26740d61b33015ff0ce1a19ee4a59b8
Johcel Gene T. Bitara <genebitara@gmail.com>
2024-01-14 20:31:02 +0800

    feat: add progress bar and toggle tree

--- src/pages/todo/features/Tree/Tree.tsx
+++ src/pages/todo/features/Tree/Tree.tsx
@@ -6,25 +6,24 @@ import TaskContext from "@pages/todo/contexts/TaskContext"
 function Tree() {
   const context = useContext(TaskContext)
   const finishedTasks = context?.tasks.filter((task) => task.done)
-
-  const points = finishedTasks?.length ?? 0
+  const points = Math.round(((finishedTasks?.length ?? 0) / (context?.tasks.length ?? 0)) * 100)
 
   const [treeGrowth, setTreeGrowth] = useState(tree20)
 
   useEffect(() => {
     function growth() {
-      if (points >= 0 && points <= 5) setTreeGrowth(tree20)
-      if (points >= 6 && points <= 10) setTreeGrowth(tree40)
-      if (points >= 11 && points <= 20) setTreeGrowth(tree60)
-      if (points >= 21 && points <= 30) setTreeGrowth(tree80)
-      if (points >= 31 && points <= 40) setTreeGrowth(tree100)
+      if (points >= 0 && points <= 20) setTreeGrowth(tree20)
+      if (points >= 21 && points <= 40) setTreeGrowth(tree40)
+      if (points >= 41 && points <= 60) setTreeGrowth(tree60)
+      if (points >= 61 && points <= 80) setTreeGrowth(tree80)
+      if (points >= 81 && points <= 100) setTreeGrowth(tree100)
     }
     growth()
   }, [context?.tasks, points])
 
   return (
     <>
-      <img src={treeGrowth} alt="Tree" className="mx-auto mb-5 w-max" />
+      <img src={treeGrowth} alt="Tree" className="mx-auto mb-5 w-[25%]" />
     </>
   )
 }
--- src/pages/todo/index.tsx
+++ src/pages/todo/index.tsx
@@ -1,41 +1,74 @@
 import { useEffect, useState } from "react"
 
+import logo from "@assets/brands/64x64.png"
 import { getTasksFromLocalStorage } from "@pages/todo/utils"
 import HomeLayout from "@layouts/HomeLayout"
 import TaskContext from "@pages/todo/contexts/TaskContext"
 import { TasksList, TasksListFinished } from "@pages/todo/components"
 import Task from "@pages/todo/types/interfaces/task"
 import { CreateTask, Tree } from "@pages/todo/features"
+import { Eye, EyeOff } from "lucide-react"
+import { Button } from "@components/ui"
 
 const TodoPage = () => {
   const [tasks, setTasks] = useState<Task[]>([])
+  const [hideTree, setHideTree] = useState(false)
+
   const unFinishedTasks = tasks.filter((task) => !task.done)
   const finishedTasks = tasks.filter((task) => task.done)
+
+  // progress bar
+  const completionPercentage = Math.round((finishedTasks.length / tasks.length) * 100)
 
   useEffect(() => {
     const tasksFromLocalStorage: Task[] = getTasksFromLocalStorage()
     setTasks(tasksFromLocalStorage)
   }, [])
 
+  const toggleTree = () => setHideTree(!hideTree)
+
   return (
     <>
       <HomeLayout>
         <div className="container">
+          <header className="w-max mx-auto mt-5">
+            <div className="flex gap-2 items-center">
+              <h2 className="scroll-m-20 font-inter-900 text-3xl font-semibold tracking-tight">Plantasker</h2>
+              <img src={logo} alt="" />
+            </div>
+            <div className="flex items-center gap-1 mx-auto w-max">
+              <div className="flex gap-3">
+                <span className="text-slate-500">{completionPercentage}%</span>
+                <span className="text-slate-500">•</span>
+              </div>
+              <Button onClick={toggleTree} size={"sm"} variant={"ghost"} className="gap-2 px-2 text-slate-500">
+                {hideTree ? (
+                  <>
+                    <EyeOff /> Show Tree
+                  </>
+                ) : (
+                  <>
+                    <Eye /> Hide Tree
+                  </>
+                )}
+              </Button>
+            </div>
+          </header>
           <TaskContext.Provider value={{ tasks, setTasks }}>
-            <Tree />
-            <div className="flex flex-col justify-center gap-5 mx-auto w-max">
+            {hideTree ? null : <Tree />}
+            <div className="flex flex-col justify-center gap-5 mt-5 mx-auto w-max">
               <CreateTask />
               <div className="flex flex-col">
                 <div className="flex justify-between">
-                  <label className="text-sm opacity-75 w-max">Tasks</label>
-                  <label className="text-sm opacity-75 w-max">{unFinishedTasks.length} Remaining Task(s)</label>
+                  <label className="text-sm text-slate-500 w-max">Tasks</label>
+                  <label className="text-sm text-slate-500 w-max">{unFinishedTasks.length} Remaining Task(s)</label>
                 </div>
                 <TasksList />
               </div>
               <div className="flex flex-col">
                 <div className="flex justify-between">
-                  <label className="text-sm opacity-75 w-max">Finished Tasks</label>
-                  <label className="text-sm opacity-75 w-max">{finishedTasks.length} Done</label>
+                  <label className="text-sm text-slate-500 w-max">Finished Tasks</label>
+                  <label className="text-sm text-slate-500 w-max">{finishedTasks.length} Done</label>
                 </div>
                 <TasksListFinished />
               </div>
