31c187167c8522f56b6933d527be7fd5985f8829
Johcel Gene T. Bitara <genebitara@gmail.com>
2024-01-14 20:57:05 +0800

    chore(deps): add dialogue

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,13 @@
 export { Button } from "@components/ui/button"
 export { Input } from "@components/ui/input"
 export { Checkbox } from "@components/ui/checkbox"
+export {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "@components/ui/dialog"
