aef3a0e620f96feb9f416d2f9d0f53ed16c1e071
genebit <genebitara@gmail.com>
2024-01-14 17:57:23 +0800

    feat: modify prompt for diff. empty states

--- src/pages/todo/components/TasksList/TasksList.tsx
+++ src/pages/todo/components/TasksList/TasksList.tsx
@@ -5,6 +5,34 @@ import TaskContext from "@pages/todo/contexts/TaskContext"
 function TasksList() {
   const context = useContext(TaskContext)
   const tasks = context?.tasks.filter((task) => !task.done)
+  const finishedTasks = context?.tasks.filter((task) => task.done)
+
+  const Prompt = () => {
+    const taskLength = tasks?.length ?? 0
+    const finishedTaskLength = finishedTasks?.length ?? 0
+
+    let component = undefined
+    if (context?.tasks.length === 0) {
+      // this user has no tasks yet.
+      component = (
+        <p className="absolute text-sm left-[2rem] top-1/4 text-center animate-fade-in">
+          Start writing tasks and marvel at your beautiful tree! 🌱
+        </p>
+      )
+    }
+    if (taskLength == 0 && finishedTaskLength != 0) {
+      // this user has finished all tasks and all are placed
+      // on the finished tasks list.
+      component = (
+        <p className="absolute text-sm left-[2rem] top-1/4 text-center animate-fade-in">
+          Good job! you've completed all your tasks for today 💪 <br />
+          Now marvel at your beautiful tree!
+        </p>
+      )
+    }
+
+    return component
+  }
 
   return (
     <>
@@ -12,12 +40,7 @@ function TasksList() {
         {tasks?.map((task) => {
           return <TaskCard key={task.id} task={task} />
         })}
-        {tasks?.length === 0 ? (
-          <p className="absolute text-sm left-[2rem] top-1/4 text-center">
-            Good job! you've completed all your tasks for today 💪 <br />
-            Now marvel at your beautiful tree!
-          </p>
-        ) : null}
+        <Prompt />
       </div>
     </>
   )
