b50e68f4115f085a9f424d0d1ed888d384be40e1
genebit <genebitara@gmail.com>
2024-01-14 18:26:57 +0800

    chore: used env for firebase config (renewed proj. keys)

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,11 +3,11 @@ export const trashSFX = "/audio/trash-sfx.mp3"
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyC4Jko8KkTDFZCkV0wHt2PfInHFM9gOw_s",
-  authDomain: "gb-plantasker.firebaseapp.com",
-  projectId: "gb-plantasker",
-  storageBucket: "gb-plantasker.appspot.com",
-  messagingSenderId: "1056064395667",
-  appId: "1:1056064395667:web:adb80b20f245f068aa3066",
-  measurementId: "G-6ZL11EENCR",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
